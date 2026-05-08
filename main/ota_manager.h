d40e1099634457d01103f9f163a4802af4bda561
huybui <huysn11b@gmail.com>
2026-05-09 01:12:36 +0700

    test: OTA integration with latest release URL and version update

--- main/ota_manager.h
+++ main/ota_manager.h
@@ -1,7 +1,7 @@
 #ifndef OTA_MANAGER_H
 #define OTA_MANAGER_H
 
-#define OTA_URL "https://github.com/huybq17/ESP32-Advanced-OTA-Project/releases/download/v1.0.1/ota_v0.2.bin"
+#define OTA_URL "https://github.com/huybq17/ESP32-Advanced-OTA-Project/releases/latest/download/OTA_ESP32.bin"
 
 // Initialize OTA Manager (Listens to system events)
 void ota_manager_init(void);
